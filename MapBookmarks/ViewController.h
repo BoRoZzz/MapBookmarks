f5b475fe2813ba3cd302cf2a6b6eae6eff22980b
BoRoZzz <kh.borys@gmail.com>
2015-10-15 17:11:52 +0300

    Initial Commit

---
+++ MapBookmarks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapBookmarks
+//
+//  Created by Borys Khliebnikov on 10/15/15.
+//  Copyright © 2015 Borys Khliebnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
